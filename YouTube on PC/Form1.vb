a53d4bcdbfc32d02705bb0b0f82360e4ea5604e4
LegitDax009 <38143460+LegitDax009@users.noreply.github.com>
2021-10-16 15:45:55 -0500

    Add project files.

---
+++ YouTube on PC/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        FormBorderStyle = FormBorderStyle.None
+        WindowState = FormWindowState.Maximized
+        Dim task = WebView21.EnsureCoreWebView2Async()
+    End Sub
+
+    Private Sub CoreWebView2InitializationCompleted(sender As Object, e As EventArgs) Handles WebView21.CoreWebView2InitializationCompleted
+        WebView21.CoreWebView2.Settings.UserAgent = "SMART-TV; Tizen 4.0"
+        WebView21.CoreWebView2.Navigate("https://www.youtube.com/tv#")
+    End Sub
+    Private Sub NavigationStarting(sender As Object, e As EventArgs) Handles WebView21.NavigationStarting
+        If WebView21.Source.ToString().Contains("https://www.youtube.com/tv#") Then
+            WebView21.CoreWebView2.Navigate("https://www.youtube.com/tv/")
+        End If
+    End Sub
+End Class
